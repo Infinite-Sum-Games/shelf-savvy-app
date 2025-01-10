f306b979c2764ee9fcb01d6835734fb85bfaf190
Vijay SB <vijaysb2006@gmail.com>
2025-01-11 03:16:41 +0530

    Profile integration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,10 +24,11 @@ function Login() {
 
       const data = await response.json();
       const token = data.token;
-
+      const email = data.email
+      console.log("email fetched:"+email)
       // Store token in localStorage
       localStorage.setItem('authToken', token);
-
+      localStorage.setItem('email',email);
       // Navigate to a protected route (e.g., /profile)
       navigate('/profile');
     } catch (err) {
@@ -36,65 +37,74 @@ function Login() {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
-      <div className="w-full max-w-md bg-gray-800 p-8 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-semibold text-white text-center mb-6">Login</h2>
-        {error && (
-          <div className="mb-4 text-sm text-red-500 text-center">{error}</div>
-        )}
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label
-              htmlFor="text"
-              className="block text-sm font-medium text-white mb-2"
-            >
-              Email Address
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setusername(e.target.value)}
-              className="w-full px-4 py-3 rounded-lg border border-gray-600 bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-white mb-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-3 rounded-lg border border-gray-600 bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300"
-          >
-            Log In
-          </button>
-        </form>
-        <p className="text-gray-400 text-center mt-6">
-          Don't have an account?{' '}
-          <a
-            href="/register"
-            className="text-blue-500 hover:underline"
-          >
-            Sign Up
-          </a>
-        </p>
+    <div className="h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-black">
+  <div className="w-full max-w-md bg-gray-800 p-8 rounded-lg shadow-xl">
+    <h2 className="text-4xl font-bold text-white text-center mb-8 tracking-wide">
+      Welcome Back
+    </h2>
+    {error && (
+      <div className="mb-4 text-sm text-red-500 text-center font-medium">
+        {error}
       </div>
-    </div>
+    )}
+    <form onSubmit={handleLogin}>
+      {/* Email Field */}
+      <div className="mb-6">
+        <label
+          htmlFor="username"
+          className="block text-sm font-medium text-gray-300 mb-2"
+        >
+          UserName
+        </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+          className="w-full px-4 py-3 rounded-lg border border-gray-700 bg-gray-900 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          placeholder="Enter your Username"
+          required
+        />
+      </div>
+      {/* Password Field */}
+      <div className="mb-6">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-300 mb-2"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full px-4 py-3 rounded-lg border border-gray-700 bg-gray-900 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          placeholder="Enter your password"
+          required
+        />
+      </div>
+      {/* Submit Button */}
+      <button
+        type="submit"
+        className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 shadow-md transform hover:scale-105"
+      >
+        Log In
+      </button>
+    </form>
+    {/* Additional Links */}
+    <p className="text-gray-400 text-center mt-6">
+      Don't have an account?{' '}
+      <a
+        href="/register"
+        className="text-blue-500 hover:underline font-medium"
+      >
+        Sign Up
+      </a>
+    </p>
+  </div>
+</div>
+
   );
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,54 +1,107 @@
 import React, { useState } from "react";
 import { Trophy, Clipboard, Package, Tag, Copy } from "lucide-react";
-
+import { PROFILE_URL } from "../constants";
+import { useEffect } from "react";
 // Mock user data (for testing purposes)
-const mockUserData = {
-  firstName: "John",
-  lastName: "Doe",
-  username: "johndoe123",
-  email: "johndoe@example.com",
-  profilePictureURL: "https://randomuser.me/api/portraits/men/1.jpg",
-  myReferralCode: "XYZ123",
-  streak: 5,
-  achievements: [
-    { id: 1, badge: "NewRecruit", createdAt: "2024-12-01T10:00:00Z" },
-    {
-      id: 2,
-      badge: "LeaderboardChallenger",
-      createdAt: "2024-12-15T14:00:00Z",
-    },
-    { id: 3, badge: "RecipeMaster", createdAt: "2025-01-05T08:00:00Z" },
-  ],
-  foodDonations: [
-    {
-      id: 1,
-      content: "5 kg of rice",
-      approval: true,
-      receivedFood: true,
-      receiverBankId: "abc123",
-    },
-    {
-      id: 2,
-      content: "10 cans of beans",
-      approval: false,
-      receivedFood: false,
-      receiverBankId: "xyz456",
-    },
-  ],
-  totalPoints: 250,
-  points: [
-    { id: 1, point: 50, createdAt: "2025-01-01T10:00:00Z" },
-    { id: 2, point: 100, createdAt: "2025-01-05T14:00:00Z" },
-    { id: 3, point: 100, createdAt: "2025-01-09T08:00:00Z" },
-  ],
-  referrals: [
-    { id: 1, joineeId: "jane123", referralCode: "ABC123" },
-    { id: 2, joineeId: "jack456", referralCode: "DEF456" },
-  ],
-};
+// const mockUserData = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   username: "johndoe123",
+//   email: "johndoe@example.com",
+//   profilePictureURL: "https://randomuser.me/api/portraits/men/1.jpg",
+//   myReferralCode: "XYZ123",
+//   streak: 5,
+//   Achievements: [
+//     { id: 1, badge: "NewRecruit", createdAt: "2024-12-01T10:00:00Z" },
+//     {
+//       id: 2,
+//       badge: "LeaderboardChallenger",
+//       createdAt: "2024-12-15T14:00:00Z",
+//     },
+//     { id: 3, badge: "RecipeMaster", createdAt: "2025-01-05T08:00:00Z" },
+//   ],
+//   foodDonations: [
+//     {
+//       id: 1,
+//       content: "5 kg of rice",
+//       approval: true,
+//       receivedFood: true,
+//       receiverBankId: "abc123",
+//     },
+//     {
+//       id: 2,
+//       content: "10 cans of beans",
+//       approval: false,
+//       receivedFood: false,
+//       receiverBankId: "xyz456",
+//     },
+//   ],
+//   totalPoints: 250,
+//   points: [
+//     { id: 1, point: 50, createdAt: "2025-01-01T10:00:00Z" },
+//     { id: 2, point: 100, createdAt: "2025-01-05T14:00:00Z" },
+//     { id: 3, point: 100, createdAt: "2025-01-09T08:00:00Z" },
+//   ],
+//   referrals: [
+//     { id: 1, joineeId: "jane123", referralCode: "ABC123" },
+//     { id: 2, joineeId: "jack456", referralCode: "DEF456" },
+//   ],
+// };
+
 
 const ProfilePage = () => {
-  const [user, setUser] = useState(mockUserData);
+    const [user, setUser] = useState({}); // Initial state set to null
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const fetchUserData = async () => {
+        try {
+            const email = localStorage.getItem("email"); // Get email from local storage
+            const token = localStorage.getItem("authToken"); // Get token from local storage
+            if (!email) throw new Error("Email not found in local storage");
+            if (!token) throw new Error("Authorization token not found in local storage");
+    
+            const response = await fetch(PROFILE_URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`, // Add Bearer token to headers
+                },
+                body: JSON.stringify({ email }), // Send email in request body
+            });
+    
+            // Check if the response status is OK (200)
+            if (!response.ok) {
+                const errorText = await response.text();
+                console.error("Server Error:", errorText);
+                throw new Error(`Error: ${response.status} ${response.statusText}`);
+            }
+
+            const contentType = response.headers.get("Content-Type");
+            if (contentType && contentType.includes("application/json")) {
+                const data = await response.json();
+                setUser(data.data); 
+                console.log(data)
+                console.log("User"+user)
+                console.log("Email: " + (user.email || "Email not available"));
+                
+
+
+            } else {
+                const text = await response.text(); // Log raw response
+                console.error("Response is not JSON, received:", text);
+                throw new Error("Response is not JSON");
+            }
+        } catch (err) {
+            console.error(err.message);
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+    
+      useEffect(() => {
+        fetchUserData();
+      }, []);
 
   const copyToClipboard = (text) => {
     navigator.clipboard
@@ -63,106 +116,112 @@ const ProfilePage = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-6 space-y-2 max-h-[90vh] overflow-y-scroll">
-      {/* Profile Header */}
-      <div className="bg-gradient-to-r from-blue-500 to-teal-500 p-6 rounded-lg shadow-lg text-white flex items-center space-x-6">
-        <img
-          src={user.profilePictureURL || "/default-profile.jpg"}
-          alt="Profile"
-          className="w-24 h-24 rounded-full border-2 border-white"
-        />
-        <div>
-          <h1 className="text-3xl font-semibold">
-            {user.firstName} {user.lastName}
-          </h1>
-          <p className="text-sm">{user.email}</p>
-          <p className="text-sm font-semibold">Points : {user.totalPoints}</p>
-          <div className="mt-1 flex items-center space-x-2">
-            <p className="text-sm">Referral Code: {user.myReferralCode}</p>
-            <button
-              className="bg-gray-200 text-gray-800 px-3 py-1 rounded-md text-sm"
-              onClick={() => copyToClipboard(user.myReferralCode)}
-            >
-              <Copy size={16} />
-            </button>
+      {/* Check if user is loaded */}
+      {user ? (
+        <>
+          {/* Profile Header */}
+          <div className="bg-gradient-to-r from-blue-500 to-teal-500 p-6 rounded-lg shadow-lg text-white flex items-center space-x-6">
+            <img
+              src={user.profilePictureURL || "/default-profile.jpg"} // Fallback image
+              alt="Profile"
+              className="w-24 h-24 rounded-full border-2 border-white"
+            />
+            <div>
+              <h1 className="text-3xl font-semibold">
+                {user.firstName} {user.lastName}
+              </h1>
+              <p className="text-sm">{user.email}</p>
+              <p className="text-sm font-semibold">Points : {user.totalPoints}</p>
+              <div className="mt-1 flex items-center space-x-2">
+                <p className="text-sm">Referral Code: {user.myReferralCode}</p>
+                <button
+                  className="bg-gray-200 text-gray-800 px-3 py-1 rounded-md text-sm"
+                  onClick={() => copyToClipboard(user.myReferralCode)}
+                >
+                  <Copy size={16} />
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-
-      {/* Achievements */}
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
-          <Trophy size={20} className="text-yellow-500" />
-          <span>Achievements</span>
-        </h2>
-        <div className="mt-4 space-y-2">
-          {user.achievements?.map((achievement) => (
-            <div
-              key={achievement.id}
-              className="bg-gray-100 rounded-lg p-4 flex items-center space-x-2"
-            >
-              <div className="flex-shrink-0">
-                <Trophy size={20} className="text-yellow-500" />
-              </div>
-              <div className="truncate">
-                <p className="text-sm text-gray-700">{achievement.badge}</p>
-                <p className="text-xs text-gray-400">
-                  {new Date(achievement.createdAt).toLocaleDateString()}
-                </p>
-              </div>
+  
+          {/* Achievements */}
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
+              <Trophy size={20} className="text-yellow-500" />
+              <span>Achievements</span>
+            </h2>
+            <div className="mt-4 space-y-2">
+              {user.Achievements?.map((Achievements) => (
+                <div
+                  key={Achievements.id}
+                  className="bg-gray-100 rounded-lg p-4 flex items-center space-x-2"
+                >
+                  <div className="flex-shrink-0">
+                    <Trophy size={20} className="text-yellow-500" />
+                  </div>
+                  <div className="truncate">
+                    <p className="text-sm text-gray-700">{Achievements.badge}</p>
+                    <p className="text-xs text-gray-400">
+                      {new Date(Achievements.createdAt).toLocaleDateString()}
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Food Donations */}
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
-          <Clipboard size={20} className="text-green-500" />
-          <span>Food Donations</span>
-        </h2>
-        <div className="mt-4 space-y-4">
-          {user.foodDonations?.map((donation) => (
-            <div
-              key={donation.id}
-              className="flex items-center justify-between p-4 border-b border-gray-200"
-            >
-              <div>
-                <p className="text-sm text-gray-700">
-                  Content: {donation.content}
-                </p>
-                <p className="text-xs text-gray-400">
-                  Status: {donation.approval ? "Approved" : "Pending"}
-                </p>
-              </div>
-              <span className="text-sm text-gray-600">
-                Received: {donation.receivedFood ? "Yes" : "No"}
-              </span>
+          </div>
+  
+          {/* Food Donations */}
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
+              <Clipboard size={20} className="text-green-500" />
+              <span>Food Donations</span>
+            </h2>
+            <div className="mt-4 space-y-4">
+              {user.foodDonations?.map((donation) => (
+                <div
+                  key={donation.id}
+                  className="flex items-center justify-between p-4 border-b border-gray-200"
+                >
+                  <div>
+                    <p className="text-sm text-gray-700">
+                      Content: {donation.content}
+                    </p>
+                    <p className="text-xs text-gray-400">
+                      Status: {donation.approval ? "Approved" : "Pending"}
+                    </p>
+                  </div>
+                  <span className="text-sm text-gray-600">
+                    Received: {donation.receivedFood ? "Yes" : "No"}
+                  </span>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Referrals */}
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
-          <Tag size={20} className="text-teal-500" />
-          <span>Referrals</span>
-        </h2>
-        <div className="mt-4 space-y-4">
-          {user.referrals?.map((referral) => (
-            <div key={referral.id} className="p-4 border-b border-gray-200">
-              <p className="text-sm text-gray-700">
-                Referred User: {referral.joineeId}
-              </p>
-              <p className="text-xs text-gray-400">
-                Referral Code: {referral.referralCode}
-              </p>
+          </div>
+  
+          {/* Referrals */}
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
+              <Tag size={20} className="text-teal-500" />
+              <span>Referrals</span>
+            </h2>
+            <div className="mt-4 space-y-4">
+              {user.Referrals?.map((referral) => (
+                <div key={referral.id} className="p-4 border-b border-gray-200">
+                  <p className="text-sm text-gray-700">
+                    Referred User: {referral.joineeId}
+                  </p>
+               
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        </>
+      ) : (
+        <div>Loading...</div> // Show loading while user data is being fetched
+      )}
     </div>
   );
+  
 };
 
 export default ProfilePage;
